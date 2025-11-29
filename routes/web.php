a81515a4778390f8767705a241b2c5406ed47736
Melpe <melpe@example.com>
2025-11-29 17:21:11 -0500

    fix: corregir respuesta HTML en rutas web y agregar test.html

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return file_get_contents(public_path('index.html'));
+    return response(file_get_contents(public_path('index.html')), 200, [
+        'Content-Type' => 'text/html; charset=utf-8'
+    ]);
 });
 
 Route::fallback(function () {
-    return file_get_contents(public_path('index.html'));
+    return response(file_get_contents(public_path('index.html')), 200, [
+        'Content-Type' => 'text/html; charset=utf-8'
+    ]);
 });
