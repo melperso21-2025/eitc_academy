dede7e9e8bf4280184e0a63f6e5eeac757b95d02
Melpe <melpe@example.com>
2025-11-29 17:12:48 -0500

    feat: configurar CORS para comunicación frontend-API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
